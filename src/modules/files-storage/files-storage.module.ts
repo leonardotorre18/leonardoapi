a67709ad627173702ecdddcb7eca636eab0565e8
Leonardotorre18 <Leonardotorre14@gmail.com>
2026-01-14 18:13:04 -0400

    refactor: comment out AzureBlobStorageService in FilesStorageModule

--- src/modules/files-storage/files-storage.module.ts
+++ src/modules/files-storage/files-storage.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AzureBlobStorageService } from './azure-blob-storage.service';
+// import { AzureBlobStorageService } from './azure-blob-storage.service';
 import { ConfigModule } from '@nestjs/config';
 import { VercelBlobStorageService } from './vercel-blob-storage.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [AzureBlobStorageService, VercelBlobStorageService],
-  exports: [AzureBlobStorageService, VercelBlobStorageService],
+  providers: [
+    // AzureBlobStorageService,
+    VercelBlobStorageService,
+  ],
+  exports: [
+    // AzureBlobStorageService,
+    VercelBlobStorageService,
+  ],
 })
 export class FilesStorageModule {}
